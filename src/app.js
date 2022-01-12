a881bd3f1bd86599ef3b489bf6a62b32d5a238ca
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-12 08:46:08 +0100

    added pics and public js

--- src/app.js
+++ src/app.js
@@ -9,25 +9,12 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// app.get('/help', (req, res) => {
-//   res.send([{
-//     name:'Andrew',
-//     age:27
-//   }, {
-//     name:'Jules'
-//   }])
-// })
-
-// app.get('/about', (req, res) => {
-//   res.send('<h1>About</h1>')
-// })
-
-// app.get('/weather', (req, res) => {
-//   res.send({
-//     forecast: 'It is snowing',
-//     location: 'Philadelphia'
-//   })
-// })
+app.get('/weather', (req, res) => {
+  res.send({
+    forecast: 'It is snowing',
+    location: 'Philadelphia'
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000.')
