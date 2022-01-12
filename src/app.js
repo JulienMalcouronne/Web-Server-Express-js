73750a0bbf6f389c14634ad865bd03e9168b814e
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-12 16:19:08 +0100

    set error 404 page and message

--- src/app.js
+++ src/app.js
@@ -50,6 +50,22 @@ app.get('/weather', (req, res) => {
   })
 })
 
+app.get('/help/*', (req, res) => {
+  res.render('404',{
+    errorMessage: "Help article not found.",
+    name: 'Julien Malcouronne',
+    title: '404'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404',{
+    errorMessage: "Page not found",
+    name: 'Julien Malcouronne',
+    title: '404'
+  })
+})
+
 app.listen(3000, () => {
   console.log('Server is up on port 3000.')
 })
