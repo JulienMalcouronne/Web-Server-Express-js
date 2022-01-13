b9be26e4653597c7a2a3eecc769e08cd1d02804b
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-13 20:56:35 +0100

    added user interaction when use of the search bar

--- public/js/app.js
+++ public/js/app.js
@@ -6,22 +6,30 @@ console.log('Client side Javascript file is loaded connect')
 //   })
 // })
 
-
+// targeting the form
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+
+// targeting the message to show to the users
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const location = search.value
 
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+
   fetch(`http://localhost:3000/weather?address=${location}`).then ((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error)
+        messageOne.textContent = data.error
       } else {
-        console.log(data.location)
-        console.log(data.forecast)
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
       }
     })
   })
